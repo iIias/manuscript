93a3f6d4868c2ab2ba9f2b579c44ded0958f23ac
Ilias May <ilias.ennmouri@mac.com>
2017-02-17 20:31:05 +0100

    Killed the NSMenu

--- Manuscript/AppDelegate.swift
+++ Manuscript/AppDelegate.swift
@@ -11,34 +11,12 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
-    let menu: NSMenu = NSMenu()
-    let modesMenu: NSMenu = NSMenu(title: "Modes 🔥")
-    let saveMenu: NSMenu = NSMenu(title: "Save 📋")
-    let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-    let statusItem = NSStatusBar.system().statusItem(withLength: -2)
+      let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
-        
-        let modesItem = NSMenuItem(title: "🔥 Modes", action: #selector(self.noFunction), keyEquivalent: "")
-        let saveItem = NSMenuItem(title: "💾 Save", action: #selector(ViewController.saveTextToDocuments), keyEquivalent: "")
-        menu.addItem(saveItem)
-        saveMenu.addItem(NSMenuItem(title: "📄 Documents", action: #selector(ViewController.saveTextToDocuments), keyEquivalent: "s"))
-        saveMenu.addItem(NSMenuItem(title: "🏠 Desktop", action: #selector(ViewController.saveTextToDesktop), keyEquivalent: ""))
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(modesItem)
-        modesMenu.addItem(NSMenuItem(title: "Light 💡", action: #selector(ViewController.setLightMode), keyEquivalent: ""))
-        modesMenu.addItem(NSMenuItem(title: "Dark 🌑", action: #selector(ViewController.setDarkMode), keyEquivalent: ""))
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(NSMenuItem(title: "❌ Quit", action: #selector(NSApp.terminate(_:)), keyEquivalent: ""))
-        modesItem.submenu = modesMenu
-        saveItem.submenu = saveMenu
-        
-        if let button = statusItem.button {
-            button.title = "✏️"
-            statusItem.menu = menu
-        }
     }
+    
     
     func noFunction() {
         print("nothing")
